4b87a4b171e38db8ef01bcf1e4a945b6ae0c4eff
Ahmed-Elzamly <aelzamly888@gmail.com>
2025-10-23 23:21:07 +0300

    Adding Token Types Enum

--- src/lexer/token_types.py
+++ src/lexer/token_types.py
@@ -1,1 +1,13 @@
 ## token categories
+from enum import Enum
+
+class TokenType(Enum):
+    KEYWORD = "Keyword"
+    IDENTIFIER = "Identifier"
+    OPERATOR = "Operator"
+    NUMERIC_CONSTANT = "Numeric Constant"
+    CHARACTER_CONSTANT = "Character Constant"
+    SPECIAL_CHARACTER = "Special Character"
+    COMMENT = "Comment"
+    WHITESPACE = "White Space"
+    NEWLINE = "Newline"
