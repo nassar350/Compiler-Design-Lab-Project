7e212b62a18e741859804321e2d878ee11352071
Mohamed Nassar <126024444+nassar350@users.noreply.github.com>
2025-10-23 19:35:52 +0300

    initializing project

---
+++ SourceCode.c
@@ -0,0 +1,13 @@
+int main() {
+    int x,y;
+    // This is a single-line comment
+    if (x == 42) {
+        /* This is
+           a block
+           comment */
+        x = x-3;
+    } else {
+        y = 3.1; // Another comment
+    }
+    return 0;
+}
